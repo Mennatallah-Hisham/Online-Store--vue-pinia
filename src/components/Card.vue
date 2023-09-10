ece556c0b1402ca0cd23522086baa88581a0fda7
unknown <mennahisham1d@gmail.com>
2023-09-10 10:29:02 +0200

    render product cards

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps(['products'])
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    
+    <article v-for="product in products" :key="product.id">
+        <img :src="product.thumbnail"/>
+        <RouterLink :to="`/product/${product.id}`">
+
+       
+        <h2>{{product.title}}</h2>
+    </RouterLink>
+        
+        <h2>{{product.category}}</h2>
+        
+    </article>
+
+</template>
+<style scoped>
+article{
+    width:320px;
+}
+img{
+    max-width: 100%;
+}
+</style>
--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted } from 'vue';
 import useProductStore from "../stores/store";
+import Card from "../components/Card.vue"
 
 
 const productStore =useProductStore();
@@ -17,12 +18,19 @@ onMounted( ()=>{
     </h1>
     <section class="flex">
 
-        {{ productStore.products }}
+    
+        <Card :products="productStore.products"/>
     </section>
 
 </template>
 <style scoped>
+.flex{
+    display: flex;
+    justify-content: center;
+    gap:2rem;
+    flex-wrap: wrap;
 
+}
 
 
 </style>
